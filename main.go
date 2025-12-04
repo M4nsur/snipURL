5cf1d03ca1baf46edf3bb5ef6578bef1d7a62022
Mansur <vakhabov.mans@gmail.com>
2025-12-04 03:38:40 +0300

    upd main

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {	
-	fmt.Println("test")
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Hello")
+}
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", hello)
+	server := http.Server{
+		Addr: ":8081",
+		Handler: router,
+	}
+	fmt.Println("server is listening on port 8081")
+	server.ListenAndServe()
 }
